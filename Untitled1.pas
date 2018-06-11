46805cfd9670726b8989c5021650720eab402ae8
Luqaiz <romerolucas28@gmail.com>
2018-06-11 19:55:44 -0300

    PRIMER ABM

--- Untitled1.pas
+++ Untitled1.pas
@@ -1,17 +1,65 @@
-program clase04del06 ;
-
-procedure limpiarPantalla ();
+program ABM;
+uses crt;
 var
-   i : integer;
+   opcion : integer;
+   seguir : char;
+   respuestaGlobal : char;
+procedure menu ();
 begin
-     for i:= 0 to 25 do
-         writeln( );
+     writeln ('1- ALTA');
+     writeln ('2- BAJA');
+     writeln ('3- MODIFICACION');
+     writeln ('4- SALIR');
+
+end;
+
+procedure alta (respuesta : char);
+begin
+     clrscr();
+     writeln ('Entro a la parte de altas');
+     writeln ('Desea volver al menu anterior? s/n');
+     readln (respuesta);
+            if (respuesta = 's') then ;
+               menu();
+     clrscr();
+end;
+procedure baja (respuesta : char);
+begin
+     clrscr();
+     writeln ('Entro a la parte de bajas');
+     writeln ('Desea volver al menu anterior? s/n');
+     readln (respuesta);
+            if (respuesta = 's') then;
+               menu();
+     clrscr();
+end;
+procedure modificacion(respuesta : char);
+begin
+     clrscr();
+     writeln ('Entro a la parte de modificaciones');
+     writeln ('Desea volver al menu anterior? s/n');
+     readln (respuesta);
+            if (respuesta = 's') then;
+               menu();
+     clrscr();
 end;
 
 begin
-     writeln ('hola');
-     readln ();
-     limpiarPantalla();
-     readln ();
+     seguir := 's';
+     respuestaglobal := 'n';
+
+     while (seguir = 's') do
+     begin
+          menu ();
+
+          readln (opcion);
+
+          case (opcion) of
+          1:alta(respuestaGlobal);
+          2:baja(respuestaGlobal);
+          3:modificacion(respuestaGlobal);
+          4:seguir := 'n';
+          end;
+    end;
 
 end.
