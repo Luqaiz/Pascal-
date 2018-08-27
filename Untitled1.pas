8aaa13594ee33a5b1253acbca289ed2cb6cfecb1
Luqaiz <romerolucas28@gmail.com>
2018-08-27 19:44:11 -0300

    MAX MATRIZ

--- Untitled1.pas
+++ Untitled1.pas
@@ -1,4 +1,15 @@
+program primerMatriz;
+const
+     matriz : array [1..3,1..3] of integer = ((23,45,68),(34,99,12),(25,78,89));
+var
+   i,j : integer;
+   mayor : integer;
 
 begin
-writeln ('hola mundo');
+     for i:= 1 to 3 do
+         for j:=1 to 3 do
+              if matriz[i,j] > mayor then
+                 mayor := matriz[i,j];
+                 writeln (mayor);
+                 readln();
 end.
